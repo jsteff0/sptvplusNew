547db40c2f3650ebde9e3fb7aed444c3d49bcad9
DroPlay20 <101497138+DroPlay20@users.noreply.github.com>
2024-02-19 21:46:19 +0200

    v0.0.28

--- src/pages/ls.tsx
+++ src/pages/ls.tsx
@@ -1,10 +1,19 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
-
+interface newsint {
+	news: Array<{ text: string, img: string }>;
+	newsVideo: Array<{ url: string, name: string, png: string }>;
+	mainNews: { title: string, text: string, img: string }
+}
 export function getStaticProps() {
   const file = path.join(process.cwd(), 'newsinfo.json');
   const data = readFileSync(file, 'utf8');
-
+  const parsedata = JSON.parse(data) as newsint
+  parsedata.news.push({
+	"text": "ЛЮБОВНИК ВЫПАЛ В ОКНО КОГДА... читать продолжение",
+	"img": "image (2).png"
+  })
+  writeFileSync(file, JSON.stringify(parsedata));
   return {
     props: {
       data,
@@ -12,6 +21,6 @@ export function getStaticProps() {
   };
 }
 
-export default function Home(props: { data: string }) {
-  return <code>{props.data}</code>;
+export default function Home(props: { data: newsint }) {
+  return <code>{props.data.news[0]?.text}</code>;
 }
